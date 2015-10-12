a99b9cd70345b1a79c825da87fc044e9bedc8bac
Atlee Brink <abrink@uoregon.edu>
2015-10-12 12:28:32 -0700

    updated some javadocs

--- VirtualPond/src/addressbook/VirtualBookIO.java
+++ VirtualPond/src/addressbook/VirtualBookIO.java
@@ -4,8 +4,8 @@ import java.io.File;
 
 public interface VirtualBookIO {
 
-	final static String VIRTUAL_ADDRESS_BOOK_EXTENSION = "pond™";
-	final static String USER_SAVETO_FOLDER_NAME = "Virtual Address Book™";
+	final static String VIRTUAL_ADDRESS_BOOK_EXTENSION = "pond";
+	final static String USER_SAVETO_FOLDER_NAME = "Virtual Address Book";
 	final static String FILE_CHARACTER_DIVIDER_REGEX = "\t"; // Tab
 	final static boolean DEFAULT_REQUIRED_FIELD_BOOL = false;
 	
